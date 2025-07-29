6f1628d34899259af22a921a17b7ba4f4eff552f
Opstree <aman.kashyap@opstree.com>
2025-07-30 00:48:16 +0530

    added structure to add features in pika pika

---
+++ src/features/feed/PokemonFeed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PokemonFeed = ()=>{
+
+    
+    return (
+        <div style={{width:'100%', height:'100%'}}>
+           pics
+        </div>
+    )
+}
+
+export default PokemonFeed;
